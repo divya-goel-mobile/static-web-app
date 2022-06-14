0962663c00db93a146d5367bb09c8cff8ecd54d5
Divya Goel <divya.goel@prudential.com.sg>
2022-06-14 16:34:07 +0530

    Checking

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Working on Azure static web app 
+        Working on Azure static web app.
         </p>
         <a
           className="App-link"
